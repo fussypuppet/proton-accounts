116526415f7beb29b0925ca7c1148f1e34b5fc73
Eliott Davidson <ulthos@gmail.com>
2020-10-01 17:43:10 -0700

    Initial commit of first photon app

---
+++ app.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"; // import from react
+
+import { Window, App, Text } from "proton-native"; // import the proton-native components
+
+export default class Example extends Component {
+  render() {
+    // all Components must have a render method
+    return (
+      <App>
+        {/* you must always include App around everything */}
+        <Window style={{ width: 300, height: 300, backgroundColor: "blue" }}>
+          <Text>Coop Accounts</Text>
+          {/* all your other components go here*/}
+        </Window>
+      </App>
+    );
+  }
+}
